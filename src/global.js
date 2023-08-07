51b524071c9ce52173675883f0ac00977c56150b
simplejeeva <skyplayjeeva@gmail.com>
2023-08-07 16:10:05 +0530

    commit

--- src/global.js
+++ src/global.js
@@ -1,3 +1,2 @@
-export const API = "http://localhost:4000";
-// export const API = "https://mongo-node-five.vercel.app";
-//
+// export const API = "http://localhost:4000";
+export const API = "https://weather-app-back.onrender.com";
